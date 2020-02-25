b0163e56400d128a632f4f81ce39003c1472aceb
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-02-24 21:33:52 -0800

    coded number to actual number conversion

--- src/main/java/com/hkadekar/anu/accountprocessor/AccountTranslator.java
+++ src/main/java/com/hkadekar/anu/accountprocessor/AccountTranslator.java
@@ -1,10 +1,44 @@
 package com.hkadekar.anu.accountprocessor;
 
+import java.util.Arrays;
+
 public class AccountTranslator {
     public static long decipherAccountNumber(char[][] codedAccountNumber){
         long accountNumber = 0;
 
         return accountNumber;
+    }
+
+
+    public static long decipherNumber(char[][] codedNumber){
+
+        String row1 = String.valueOf(codedNumber[0]);
+        String row2 = String.valueOf(codedNumber[1]);
+        String row3 = String.valueOf(codedNumber[2]);
+        String row4 = String.valueOf(codedNumber[3]);
+
+        if(row1.equals(" _ ") && row2.equals("| |") && row3.equals("|_|") && row4.equals("   ")){
+            return 0;
+        } else if(row1.equals("   ") && row2.equals("  |") && row3.equals("  |") && row4.equals("   ")){
+            return 1;
+        } else if(row1.equals(" _ ") && row2.equals(" _|") && row3.equals("|_ ") && row4.equals("   ")){
+            return 2;
+        } else if(row1.equals(" _ ") && row2.equals(" _|") && row3.equals(" _|") && row4.equals("   ")){
+            return 3;
+        } else if(row1.equals("   ") && row2.equals("|_|") && row3.equals("  |") && row4.equals("   ")){
+            return 4;
+        } else if(row1.equals(" _ ") && row2.equals("|_ ") && row3.equals(" _|") && row4.equals("   ")){
+            return 5;
+        } else if(row1.equals(" _ ") && row2.equals("|_ ") && row3.equals("|_|") && row4.equals("   ")){
+            return 6;
+        } else if(row1.equals(" _ ") && row2.equals("  |") && row3.equals("  |") && row4.equals("   ")){
+            return 7;
+        } else if(row1.equals(" _ ") && row2.equals("|_|") && row3.equals("|_|") && row4.equals("   ")){
+            return 8;
+        } else {
+            return 9;
+        }
+
     }
 
 
--- src/test/java/com/hkadekar/anu/accountprocessor/AccountTranslatorTest.java
+++ src/test/java/com/hkadekar/anu/accountprocessor/AccountTranslatorTest.java
@@ -21,4 +21,117 @@ public class AccountTranslatorTest {
         Assertions.assertEquals(expectedAccountNumber, returnedAccountNumber);
 
     }
+
+    @Test
+    void testCodedNumberConvertor(){
+        char[][] codedNumber0 = {
+                {' ', '_', ' '},
+                {'|', ' ', '|'},
+                {'|', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber1 = {
+                {' ', ' ', ' '},
+                {' ', ' ', '|'},
+                {' ', ' ', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber2 = {
+                {' ', '_', ' '},
+                {' ', '_', '|'},
+                {'|', '_', ' '},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber3 = {
+                {' ', '_', ' '},
+                {' ', '_', '|'},
+                {' ', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber4 = {
+                {' ', ' ', ' '},
+                {'|', '_', '|'},
+                {' ', ' ', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber5 = {
+                {' ', '_', ' '},
+                {'|', '_', ' '},
+                {' ', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber6 = {
+                {' ', '_', ' '},
+                {'|', '_', ' '},
+                {'|', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber7 = {
+                {' ', '_', ' '},
+                {' ', ' ', '|'},
+                {' ', ' ', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber8 = {
+                {' ', '_', ' '},
+                {'|', '_', '|'},
+                {'|', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        char[][] codedNumber9 = {
+                {' ', '_', ' '},
+                {'|', '_', '|'},
+                {' ', '_', '|'},
+                {' ', ' ', ' '}
+        };
+
+        long expectedNumber = 1;
+        long returnedNumber = AccountTranslator.decipherNumber(codedNumber1);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 0;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber0);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 2;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber2);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 3;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber3);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 4;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber4);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 5;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber5);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 6;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber6);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 7;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber7);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 8;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber8);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+
+        expectedNumber = 9;
+        returnedNumber = AccountTranslator.decipherNumber(codedNumber9);
+        Assertions.assertEquals(expectedNumber, returnedNumber);
+    }
 }
