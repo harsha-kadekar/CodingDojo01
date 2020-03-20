06825f172cf3b539d1f859a4eb1c8a4ca6a5beda
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-03-19 20:16:22 -0700

    added dummy account list file

--- src/main/java/com/hkadekar/anu/accountprocessor/AccountListFileParser.java
+++ src/main/java/com/hkadekar/anu/accountprocessor/AccountListFileParser.java
@@ -1,11 +1,45 @@
 package com.hkadekar.anu.accountprocessor;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountListFileParser {
     public static long[] getAccountListFromFile(String accountListFile){
         long[] accountNumbers = null;
 
-        
+        List<String> fileLines = new ArrayList<>();
+
+        try{
+            fileLines = Files.readAllLines(Paths.get(accountListFile), StandardCharsets.UTF_8);
+
+            for(int i = 0; i < fileLines.size(); i+=4){
+                char[] line1 = fileLines.get(i).toCharArray();
+                char[] line2 = fileLines.get(i+1).toCharArray();
+                char[] line3 = fileLines.get(i+2).toCharArray();
+                char[] line4 = fileLines.get(i+3).toCharArray();
+
+
+
+            }
+
+        }catch (IOException exp){
+            System.out.println(exp);
+            exp.printStackTrace();
+        }
 
         return accountNumbers;
+    }
+
+    private static char[][] getCodedAccountNumberAt(List<String> fileLines, int index){
+        char[][] codedAccountNumber = new char[4][];
+
+        
+
+        return codedAccountNumber;
     }
 }
