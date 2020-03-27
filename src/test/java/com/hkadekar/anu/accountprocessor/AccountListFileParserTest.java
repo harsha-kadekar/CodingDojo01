780626c4bf390bfc0873c404694f811ff94dff55
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-03-26 22:17:27 -0700

    Completed user story 1 - bankingOCR

--- src/test/java/com/hkadekar/anu/accountprocessor/AccountListFileParserTest.java
+++ src/test/java/com/hkadekar/anu/accountprocessor/AccountListFileParserTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class AccountListFileParserTest {
 
@@ -16,5 +15,52 @@ public class AccountListFileParserTest {
 
         Assertions.assertArrayEquals(expectedAccountNumbers, actualAccountNumbers);
 
+    }
+
+    @Test
+    public void testEmptyAccountListsFileReader() throws InvalidFileException, IOException, InvalidFileException {
+        String accountListFile = AccountListFileParserTest.class.getClassLoader().getResource("errorAccountList1").getPath();
+
+        Assertions.assertThrows(InvalidFileException.class, () -> {
+            AccountListFileParser.getAccountListFromFile(accountListFile);
+        });
+
+
+    }
+
+    @Test
+    public void testIncompleteAccountListsFileReader() throws InvalidFileException, IOException, InvalidFileException {
+        String accountListFile = AccountListFileParserTest.class.getClassLoader().getResource("errorAccountList2").getPath();
+
+        Assertions.assertThrows(InvalidFileException.class, () -> {
+            AccountListFileParser.getAccountListFromFile(accountListFile);
+        });
+    }
+
+    @Test
+    public void testMissingDataAccountListFilereader() throws InvalidFileException, IOException, InvalidFileException {
+        String accountListFile = AccountListFileParser.class.getClassLoader().getResource("errorAccountList3").getPath();
+
+        Assertions.assertThrows(InvalidFileException.class, () -> {
+            AccountListFileParser.getAccountListFromFile(accountListFile);
+        });
+    }
+
+    @Test
+    public void testInvalidFileExtraCharacterAccountListFileReader() throws InvalidFileException, InvalidCharacterException, IOException {
+        String accountListFile = AccountListFileParserTest.class.getClassLoader().getResource("errorAccountList4").getPath();
+
+        Assertions.assertThrows(InvalidFileException.class, () -> {
+            AccountListFileParser.getAccountListFromFile(accountListFile);
+        });
+    }
+
+    @Test
+    public void testInvalidCharacterAccountListFileReader() throws InvalidCharacterException, InvalidFileException, IOException {
+        String accountListFile = AccountListFileParserTest.class.getClassLoader().getResource("errorAccountList5").getPath();
+
+        Assertions.assertThrows(InvalidCharacterException.class, () -> {
+            AccountListFileParser.getAccountListFromFile(accountListFile);
+        });
     }
 }
